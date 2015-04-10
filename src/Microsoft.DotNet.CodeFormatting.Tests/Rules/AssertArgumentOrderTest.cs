8b667392442968f642bbc9b71bdd7518994828cc
Chad Nedzlek <chadnedz@microsoft.com>
2015-04-09 18:11:34 -0700

    Rearrange Assert.Equals inverted arguments
    If the "actual" argument is a constant, and the "expected" is not,
    that is almost certainly a mistake in coding.  Though it has no
    semantic difference (it will fail or pass the same either way),
    the messaging and maintenance is better if they are corrected.
    
    Fixes #105

---
+++ src/Microsoft.DotNet.CodeFormatting.Tests/Rules/AssertArgumentOrderTest.cs
@@ -0,0 +1,263 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Remoting;
+using Microsoft.CodeAnalysis;
+using Xunit;
+
+namespace Microsoft.DotNet.CodeFormatting.Tests
+{
+    public class AssertArgumentOrderTest : LocalSemanticRuleTestBase
+    {
+        internal override ILocalSemanticFormattingRule Rule
+        {
+            get { return new Rules.AssertArgumentOrderRule(); }
+        }
+
+        protected override IEnumerable<MetadataReference> GetSolutionMetadataReferences()
+        {
+            foreach (var reference in base.GetSolutionMetadataReferences())
+            {
+                yield return reference;
+            }
+
+            yield return MetadataReference.CreateFromAssembly(typeof (Assert).Assembly);
+        }
+
+        [Fact]
+        public void TestSwapInvertedEqual()
+        {
+            string source = @"
+public class Tests
+{
+    public void TestA()
+    {
+        int actual = 1;
+        Xunit.Assert.Equal(actual, 1);
+    }
+}
+";
+            string expected = @"
+public class Tests
+{
+    public void TestA()
+    {
+        int actual = 1;
+        Xunit.Assert.Equal(1, actual);
+    }
+}
+";
+
+            Verify(source, expected);
+        }
+
+        [Fact]
+        public void TestSwapInvertedEqualEnum()
+        {
+            string source = @"
+public class Tests
+{
+    private enum E
+    {
+        A,
+        B,
+    }
+
+    public void TestA()
+    {
+        E actual = E.A;
+        Xunit.Assert.Equal(actual, E.A);
+    }
+}
+";
+            string expected = @"
+public class Tests
+{
+    private enum E
+    {
+        A,
+        B,
+    }
+
+    public void TestA()
+    {
+        E actual = E.A;
+        Xunit.Assert.Equal(E.A, actual);
+    }
+}
+";
+            Verify(source, expected);
+        }
+
+        [Fact]
+        public void TestSwapInvertedEqualConstField()
+        {
+            string source = @"
+public class Tests
+{
+    private const int A;
+
+    public void TestA()
+    {
+        int actual = A;
+        Xunit.Assert.Equal(actual, A);
+    }
+}
+";
+            string expected = @"
+public class Tests
+{
+    private const int A;
+
+    public void TestA()
+    {
+        int actual = A;
+        Xunit.Assert.Equal(A, actual);
+    }
+}
+";
+            Verify(source, expected);
+        }
+
+        [Fact]
+        public void TestSwapInvertedNotEqual()
+        {
+            string source = @"
+public class Tests
+{
+    public void TestA()
+    {
+        int actual = 1;
+        Xunit.Assert.NotEqual(actual, 1);
+    }
+}
+";
+            string expected = @"
+public class Tests
+{
+    public void TestA()
+    {
+        int actual = 1;
+        Xunit.Assert.NotEqual(1, actual);
+    }
+}
+";
+            Verify(source, expected);
+        }
+
+        [Fact]
+        public void TestSwapInvertedEqualFromUsing()
+        {
+            string source = @"
+using Xunit;
+
+public class Tests
+{
+    public void TestA()
+    {
+        int actual = 1;
+        Assert.Equal(actual, 1);
+    }
+}
+";
+            string expected = @"
+using Xunit;
+
+public class Tests
+{
+    public void TestA()
+    {
+        int actual = 1;
+        Assert.Equal(1, actual);
+    }
+}
+";
+            Verify(source, expected);
+        }
+
+        [Fact]
+        public void TestIgnoredCorrectEqual()
+        {
+            string text = @"
+public class Tests
+{
+    public void TestA()
+    {
+        int actual = 1;
+        Xunit.Assert.Equal(1, actual);
+    }
+}
+";
+            Verify(text, text);
+        }
+
+        [Fact]
+        public void TestIgnoredDoubleConstEqual()
+        {
+            string text = @"
+public class Tests
+{
+    public void TestA()
+    {
+        Xunit.Assert.Equal(1, 2);
+    }
+}
+";
+            Verify(text, text);
+        }
+
+        [Fact]
+        public void TestIgnoredDoubleVariableEqual()
+        {
+            string text = @"
+public class Tests
+{
+    public void TestA()
+    {
+        int actual = 1;
+        int expected = 1;
+        Xunit.Assert.Equal(actual, expected);
+    }
+}
+";
+            Verify(text, text);
+        }
+
+        [Fact]
+        public void TestIgnoredCorrectNotEqual()
+        {
+            string text = @"
+public class Tests
+{
+    public void TestA()
+    {
+        int actual = 1;
+        Xunit.Assert.NotEqual(1, actual);
+    }
+}
+";
+            Verify(text, text);
+        }
+
+        [Fact]
+        public void TestIgnoreOtherAssert()
+        {
+            string text = @"
+public class Assert
+{
+    public void Equal(int expected, int actual)
+    {
+    }
+}
+
+public class Tests
+{
+    public void TestA()
+    {
+        int actual = 1;
+        Assert.NotEqual(1, actual);
+    }
+}
+";
+            Verify(text, text);
+        }
+    }
+}
---
+++ src/Microsoft.DotNet.CodeFormatting/Rules/AssertArgumentOrderRule.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Microsoft.DotNet.CodeFormatting.Rules
+{
+    internal class AssertArgumentOrderRule : ILocalSemanticFormattingRule
+    {
+        private class Rewriter : CSharpSyntaxRewriter
+        {
+            private readonly SemanticModel _model;
+
+            private static readonly HashSet<string> TargetMethods =
+                new HashSet<string>(StringComparer.Ordinal)
+                {
+                    "Xunit.Assert.Equal",
+                    "Xunit.Assert.NotEqual",
+                };
+
+            private static readonly HashSet<string> ActualParamterNames =
+                new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+                {
+                    "actual",
+                };
+
+            private static readonly HashSet<string> ExpectedParamterNames =
+                new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+                {
+                    "expected",
+                };
+
+            public Rewriter(SemanticModel model)
+            {
+                _model = model;
+            }
+
+            public override SyntaxNode VisitInvocationExpression(InvocationExpressionSyntax node)
+            {
+                var symbol = _model.GetSymbolInfo(node.Expression).Symbol as IMethodSymbol;
+                if (symbol == null || !TargetMethods.Contains(GetFullName(symbol)))
+                {
+                    return base.VisitInvocationExpression(node);
+                }
+
+                int actualIndex = ParameterWithNameIndex(symbol, ActualParamterNames);
+                int expectedIndex = ParameterWithNameIndex(symbol, ExpectedParamterNames);
+
+                if (actualIndex == -1 || expectedIndex == -1)
+                {
+                    return base.VisitInvocationExpression(node);
+                }
+
+                var argumentList = (ArgumentListSyntax) Visit(node.ArgumentList);
+
+                if (!IsConstant(argumentList.Arguments[actualIndex].Expression) ||
+                    IsConstant(argumentList.Arguments[expectedIndex].Expression))
+                {
+                    return base.VisitInvocationExpression(node);
+                }
+
+                List<ArgumentSyntax> arguments = argumentList.Arguments.ToList();
+                ArgumentSyntax actualArgument = arguments[actualIndex];
+                ArgumentSyntax expectedArgument = arguments[expectedIndex];
+                arguments[actualIndex] = expectedArgument;
+                arguments[expectedIndex] = actualArgument;
+                
+                return node.Update(
+                    (ExpressionSyntax) Visit(node.Expression),
+                    argumentList.WithArguments(SyntaxFactory.SeparatedList(arguments)));
+            }
+
+            private bool IsConstant(ExpressionSyntax expression)
+            {
+                switch (expression.Kind())
+                {
+                    case SyntaxKind.CharacterLiteralExpression:
+                    case SyntaxKind.FalseLiteralExpression:
+                    case SyntaxKind.NullLiteralExpression:
+                    case SyntaxKind.NumericLiteralExpression:
+                    case SyntaxKind.StringLiteralExpression:
+                    case SyntaxKind.TrueLiteralExpression:
+                    {
+                        return true;
+                    }
+
+                    case SyntaxKind.SimpleMemberAccessExpression:
+                    case SyntaxKind.IdentifierName:
+                        {
+                        var symbol = _model.GetSymbolInfo(expression).Symbol;
+
+                        if (symbol?.Kind == SymbolKind.Field)
+                        {
+                            return ((IFieldSymbol) symbol).IsConst;
+                        }
+
+                        break;
+                    }
+                }
+
+                return false;
+            }
+
+            private int ParameterWithNameIndex(IMethodSymbol symbol, HashSet<string> names)
+            {
+                for (int i = 0; i < symbol.Parameters.Length; i++)
+                {
+                    if (names.Contains(symbol.Parameters[i].Name))
+                    {
+                        return i;
+                    }
+                }
+
+                return -1;
+            }
+
+            private static string GetFullName(IMethodSymbol symbol)
+            {
+                return GetFullName(symbol.ContainingType) + "." + symbol.Name;
+            }
+
+            private static string GetFullName(INamedTypeSymbol type)
+            {
+                if (type.ContainingType != null)
+                {
+                    return GetFullName(type.ContainingType) + "." + type.Name;
+                }
+
+                return GetFullName(type.ContainingNamespace) + "." + type.Name;
+            }
+
+            private static string GetFullName(INamespaceSymbol namespaceSymbol)
+            {
+                if (namespaceSymbol.ContainingNamespace != null &&
+                    !namespaceSymbol.ContainingNamespace.IsGlobalNamespace)
+                {
+                    return GetFullName(namespaceSymbol.ContainingNamespace) + "." + namespaceSymbol.Name;
+                }
+
+                return namespaceSymbol.Name;
+            }
+        }
+
+        public bool SupportsLanguage(string languageName)
+        {
+            return languageName == LanguageNames.CSharp;
+        }
+
+        public async Task<SyntaxNode> ProcessAsync(Document document, SyntaxNode syntaxRoot, CancellationToken cancellationToken)
+        {
+            Rewriter rewriter = new Rewriter(await document.GetSemanticModelAsync(cancellationToken));
+            return rewriter.Visit(await document.GetSyntaxRootAsync(cancellationToken));
+        }
+    }
+}
